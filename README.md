8153d267fe2a2a3af149ade66f265aace0d24d1c
Victor Pereira <victordiaspereira250@gmail.com>
2022-04-24 21:21:38 -0300

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# NLW-7
+# NLW-7 | Heat
  
+Project made following the NLW-7 full stack web application with React-ts, Node, Express-ts and Postgres
+
+#
+
+# <img width="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" /> <img width="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" /> <img width="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" /> <img width="100" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/authenticate_user_controller";
 import { CreateMessageController } from "./controllers/create_message_controller";
 import { GetLast3MessageController } from "./controllers/get_3last_message_controller";
-import { ProfileUserController } from "./controllers/profile_user_contorller";
+import { ProfileUserController } from "./controllers/profile_user_controller";
 import { ensureAuthenticated } from "./middleware/ensure_authenticated";
 
 const router = Router();
@@ -18,5 +18,15 @@ router.post(
 router.get("/message/last3", new GetLast3MessageController().handle);
 
 router.get("/profile", ensureAuthenticated,new ProfileUserController().handle)
+
+router.get("/github", (request, response)=>{
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+router.get("/signin/callback",(request, response)=>{
+    const { code } = request.query;
+    
+    return response.json(code);
+});
 
 export { router };
